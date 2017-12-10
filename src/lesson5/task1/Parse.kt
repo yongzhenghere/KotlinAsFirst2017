46861633366f6b3b9eae49bd38b52492c4ddc7ca
yongzhengzhao <happyguyszyz@gmail.com>
2017-12-10 15:38:06 +0300

    lesson 6-2,7-1

--- src/lesson5/task1/Parse.kt
+++ src/lesson5/task1/Parse.kt
@@ -262,6 +262,11 @@ fun mostExpensive(description: String): String {
  * Вернуть -1, если roman не является корректным римским числом
  */
 fun fromRoman(roman: String): Int = TODO()
+//{
+//    val map = mapOf(1 to "I", 4 to "IV", 5 to "V", 9 to "IX", 10 to "X", 40 to "XL", 50 to "L",
+//            90 to "XC", 100 to "C", 400 to "CD", 500 to "D", 900 to "CM", 1000 to "M")
+//
+//}
 
 /**
  * Очень сложная
